a79990c0a5f246dd580d144716ccb19ec60ad676
Daniel Shin <shin.daniel.d@gmail.com>
2016-10-23 03:06:32 -0600

    final stlying touches

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( npm.js )
