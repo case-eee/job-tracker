96124d07fb353a2a08cc1dc2bb6b79e8e36e0ab1
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-15 15:34:13 -0700

    Sad path tests for delete and update companies.

--- spec/features/companies/user_creates_new_company_spec.rb
+++ spec/features/companies/user_creates_new_company_spec.rb
@@ -1,14 +1,23 @@
 require 'rails_helper'
 
 describe "User creates a new company" do
-  scenario "a user can create a new company" do
+  scenario "with valid attributes" do
     visit new_company_path
 
     fill_in "company[name]", with: "ESPN"
     click_button "Create"
 
-    expect(current_path).to eq("/companies/#{Company.last.id}/jobs")
-    expect(page).to have_content("ESPN")
-    expect(Company.count).to eq(1)
+    expect(current_path).to eq company_jobs_path(Company.last)
+    expect(page).to have_content "ESPN" 
+    expect(Company.count).to eq 1
+  end
+
+  scenario "without valid attributes" do
+    visit new_company_path
+
+    click_button "Create"
+
+    expect(page).to have_content "Name can't be blank"
+  
   end
 end
--- spec/features/companies/user_edits_existing_company_spec.rb
+++ spec/features/companies/user_edits_existing_company_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe "User edits an existing company" do
-  scenario "a user can edit a company" do
+  scenario "with valid data" do
     company = create(:company)
     first_name = company.name
     new_name = "EA Sports"
-    
+
     visit edit_company_path(company)
 
     fill_in "company[name]", with: new_name
@@ -14,5 +14,18 @@ describe "User edits an existing company" do
     expect(current_path).to eq company_jobs_path(company)
     expect(page).to have_content new_name
     expect(page).to_not have_content first_name
+  end
+
+  scenario "with invalid data" do
+    stored_company = create(:company)
+    company = create(:company)
+    invalid_name = stored_company.name
+
+    visit edit_company_path(company)
+
+    fill_in "company[name]", with: invalid_name
+    click_button "Update"
+
+    expect(page).to have_content "Name has already been taken"
   end
 end
