36c6ba7f24a5c1caded0dc93b05a3f19a8cf4147
Robert Smith <robert.smith160@icloud.com>
2016-12-15 20:27:29 -0700

    "Updates model tests."

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,31 +2,18 @@ require 'rails_helper'
 
 describe Company do
   describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a name" do
-        company = Company.new()
-        expect(company).to be_invalid
+      context "it must have a unique name" do
+        it {should validate_uniqueness_of(:name)}
       end
-
-      it "has a unique name" do
-        Company.create(name: "Dropbox")
-        company = Company.new(name: "Dropbox")
-        expect(company).to be_invalid
-      end
-    end
 
     context "valid attributes" do
-      it "is valid with a name" do
-        company = Company.new(name: "Dropbox")
-        expect(company).to be_valid
-      end
+      it {should validate_presence_of(:name)}
     end
   end
 
   describe "relationships" do
-    it "has many jobs" do
-      company = Company.new(name: "Dropbox")
-      expect(company).to respond_to(:jobs)
+    context "has many jobs" do
+      it {should have_many(:jobs)}
     end
   end
 end
