c2541b189381b4966b571346b36ce7aafc6a7377
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-16 00:54:52 -0700

    Initial refactor of old code complete, jobs functionality expanded with Editing, including testing error cases

--- spec/features/jobs/user_can_edit_existing_job_details_spec.rb
+++ spec/features/jobs/user_can_edit_existing_job_details_spec.rb
@@ -9,14 +9,75 @@ describe "User can edit details of existing job" do
     new_job_level_of_interest = "80"
     new_job_city = "Denver, CO"
 
-    visit edit_company_job_path(company, job)
+    visit company_job_path(company, job)
+
+    click_on "Edit"
 
     fill_in "job[title]", with: new_job_title
     fill_in "job[description]", with: new_job_description
     fill_in "job[level_of_interest]", with: new_job_level_of_interest
     fill_in "job[city]", with: new_job_city
 
-    click_button "Update"
+    click_on "Update"
+
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}")
+    expect(Job.count).to eq(1)
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(new_job_title)
+    expect(page).to have_content(new_job_description)
+    expect(page).to have_field("job_city", with: "#{new_job_city}", disabled: true)
+    expect(page).to have_field("job_level_of_interest", with: "#{new_job_level_of_interest}", disabled: true)
+  end
+  scenario "except when required fields changed to empty" do
+    company = Company.create(name: "ESPN")
+    job = Job.create(title: "Job Title", description: "Job test description", level_of_interest: 10, city: "Phoenix", company: company)
+    new_job_title = "Developer"
+    new_job_description = "So fun!"
+    new_job_level_of_interest = "80"
+    new_job_city = "Denver, CO"
+
+    visit company_job_path(company, job)
+
+    click_on "Edit"
+
+    fill_in "job[title]", with: ""
+    fill_in "job[description]", with: new_job_description
+    fill_in "job[level_of_interest]", with: ""
+    fill_in "job[city]", with: ""
+
+    click_on "Update"
+
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}")
+    expect(Job.count).to eq(1)
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Level of interest can't be blank")
+    expect(page).to have_content("City can't be blank")
+  end
+  scenario "when completes all fields after error messages" do
+    company = Company.create(name: "ESPN")
+    job = Job.create(title: "Job Title", description: "Job test description", level_of_interest: 10, city: "Phoenix", company: company)
+    new_job_title = "Developer"
+    new_job_description = "So fun!"
+    new_job_level_of_interest = "80"
+    new_job_city = "Denver, CO"
+
+    visit company_job_path(company, job)
+
+    click_on "Edit"
+
+    fill_in "job[title]", with: ""
+    fill_in "job[description]", with: new_job_description
+    fill_in "job[level_of_interest]", with: ""
+    fill_in "job[city]", with: ""
+
+    click_on "Update"
+
+    fill_in "job[title]", with: new_job_title
+    fill_in "job[description]", with: new_job_description
+    fill_in "job[level_of_interest]", with: new_job_level_of_interest
+    fill_in "job[city]", with: new_job_city
+
+    click_on "Update"
 
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}")
     expect(Job.count).to eq(1)
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 describe "User sees a specific job" do
   scenario "a user sees a job for a specific company" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job = Job.create(title: "Job Title", description: "Job test description", level_of_interest: 10, city: "Phoenix", company: company)
 
     visit company_job_path(company, job)
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("70")
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}")
+    expect(Job.count).to eq(1)
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(job.title)
+    expect(page).to have_content(job.description)
+    expect(page).to have_field("job_city", with: "#{job.city}", disabled: true)
+    expect(page).to have_field("job_level_of_interest", with: "#{job.level_of_interest}", disabled: true)
   end
 end
