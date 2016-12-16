78cc487cbebf5b7fc1a208d2b15f5b0b792e08cb
Daniel Rodriguez <drod2793@gmail.com>
2016-12-16 04:05:57 -0700

    Adds user succesfully edits category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,6 +17,20 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "You updated #{@category.title}"
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def category_params
