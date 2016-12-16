592cc2dbc4176a129d8ce1df3219582e027a1e99
Robert Smith <robert.smith160@icloud.com>
2016-12-16 15:23:50 -0700

    "Adds dropdown box for categories, when creating a new job."

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -5,11 +5,18 @@ describe "User creates a new job" do
     company = Company.create!(name: "ESPN")
     visit new_company_job_path(company)
 
+
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
-
+    # within "#job_category_id" do
+    #   select("option[value='2'")
+    # end
+    x = find_field("job[category_id]").value
+    # x = find_by_id("job_category_id", :text => "Software Engineering")
+    # page.select(:text => "Software Engineering", :from => "job[category_id]")
+    binding.pry
     click_button "Create"
 
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
@@ -17,6 +24,8 @@ describe "User creates a new job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("80")
     expect(page).to have_content("Denver")
+    expect(page).to have_select('job_category_id',selected: "Option 2")
+    expect(page).to have_content("Software Engineering")
   end
 
   describe "validations" do
