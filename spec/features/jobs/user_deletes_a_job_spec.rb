9ce9798988e6125fab1cf1ca910f2ef51868f8e6
Daniel Rodriguez <drod2793@gmail.com>
2016-12-15 18:37:03 -0700

    Adds job delete feature

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.feature "User deletes existing job" do
   scenario "a user can delete a job" do
     company = create(:company)
-    job_1, job_2 = create_list(:job, 2)
+    job_1 = create(:job)
+    job_2 = create(:job, title: "Something Else")
     company.jobs << [job_1, job_2]
 
     visit company_job_path(company, job_1)
 
     click_link "Delete"
 
-    expect(current_path).to eq(company_path(company))
+    expect(current_path).to eq(company_jobs_path(company))
     expect(page).to_not have_content(job_1.title)
-
     end
 end
