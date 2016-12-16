d22b935a0be87637d0a887363ec3fe506ac45145
Ethan Bennett <ebennett19@gmail.com>
2016-12-15 18:38:26 -0700

    Edit jobs functional with test

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,14 +25,16 @@ class JobsController < ApplicationController
   end
 
   def edit
+    @company = Company.find(params[:company_id])
     @job = Job.find(params[:id])
   end
 
   def update
+    @company = Company.find(params[:company_id])
     @job = Job.find(params[:id])
     if @job.update(job_params)
       flash[:success] = "#{@job.title} was successfully updated"
-      redirect_to job_path(@job)
+      redirect_to company_job_path(@company, @job)
     else
       render :edit
     end
--- spec/features/jobs/user_edits_a_job_spec.rb
+++ spec/features/jobs/user_edits_a_job_spec.rb
@@ -4,16 +4,14 @@ RSpec.feature "User can edit a job" do
   before do
     @company = Company.create(name: "Enron")
     @company.jobs << Job.create(title: "Car Washer", description: "$175k/yr", city: "Denver", level_of_interest: "100")
+    @job = Job.find_by(title: "Car Washer")
   end
 
   scenario "if all their entries are valid" do
-    job = Job.find_by(title: "Car Washer")
-    path = job.id
-    # byebug
-    visit edit_company_job_path(path)
+    visit company_job_path(@company, @job)
     click_on "Edit"
-    fill_in "title", with: "Freelance Custodian"
-    click_on "Update Artist"
+    fill_in "Title", with: "Freelance Custodian"
+    click_on "Update Job"
 
     expect(page).to have_content "Freelance Custodian"
   end
