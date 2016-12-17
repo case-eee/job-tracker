e8a166cce3dd37a88dbad55b68feaa85c51764be
Robert Smith <robert.smith160@icloud.com>
2016-12-17 16:06:44 -0700

    "Adds new functionality and tests to comments"

---
+++ spec/features/comments/user_creates_a_new_comment_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "comments#new" do
+  before do
+    @job = create(:job)
+    visit(new_company_job_comment_path(@job.company, @job))
+    fill_in "comment[content]", with: "Maybe not!"
+
+  end
+  scenario "the user can create a new comment " do
+    expect(@job.comments.count).to eq(0)
+
+    click_on("Create Comment")
+
+    expect(@job.comments.count).to eq(1)
+    expect(@job.comments.first.content).to eq("Maybe not!")
+  end
+end
