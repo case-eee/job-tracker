8cc1151cb3436a79dbeb40f3051ca7afdd69da47
Caroline Powell <carpowell10@gmail.com>
2016-12-17 11:49:34 -0700

    Finish core functionality for jobs, companies, and categories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,21 @@
 class JobsController < ApplicationController
+  # refactor: @company = Company.find(params[:company_id])
+  # refactor: @job = Job.find(params[:id])
+
   def index
     @company = Company.find(params[:company_id])
     @jobs = @company.jobs
+    render('companies/show')
   end
 
   def new
     @company = Company.find(params[:company_id])
-    @job = Job.new()
+    @job = @company.jobs.new
   end
 
   def create
     @company = Company.find(params[:company_id])
-    @job = @company.jobs.new(job_params)
+    @job = @company.jobs.create(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
       redirect_to company_job_path(@company, @job)
@@ -21,24 +25,35 @@ class JobsController < ApplicationController
   end
 
   def show
+    @company = Company.find(params[:company_id])    #Do we need this?
     @job = Job.find(params[:id])
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to company_job_path(@company, @job)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find( params[:id] )
+    @job.delete
+    redirect_to company_path(@company)
   end
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id)
   end
 end
