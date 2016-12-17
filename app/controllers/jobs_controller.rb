73b55ad35f12be1d3faa33b84706354e55ac5f57
Jason Conrad <jdconrad@liberty.edu>
2016-12-17 11:28:41 -0700

    add functionality for jobs to pass the test, also add a little stylization to the page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,6 +16,7 @@ class JobsController < ApplicationController
       flash[:success] = "You created #{@job.title} at #{@company.name}"
       redirect_to company_job_path(@company, @job)
     else
+      @errors = @job.errors.full_messages
       render :new
     end
   end
@@ -25,15 +26,27 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    if @job.save
+      flash[:success] = "#{@job.title} updated!"
+      redirect_to company_path(@job)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # implement on your own!
+    job = Job.find(params[:id])
+    job.delete
+
+    flash[:success] = "#{job.title} was successfully deleted!"
+    redirect_to company_jobs_path
   end
 
   private
