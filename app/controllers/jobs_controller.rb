77b80b6506b71f6e3758b5fd54944e4df76a842e
Jason Conrad <jdconrad@liberty.edu>
2016-12-17 17:48:13 -0700

    functionality for categories 99% complete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,6 +5,7 @@ class JobsController < ApplicationController
   end
 
   def new
+    @categories = Category.all
     @company = Company.find(params[:company_id])
     @job = Job.new()
   end
@@ -26,6 +27,7 @@ class JobsController < ApplicationController
   end
 
   def edit
+    @categories = Category.all
     @company = Company.find(params[:id])
     @job = Job.find(params[:id])
   end
