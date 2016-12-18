f29521b8c81eca0999c2d4b5995f07fd22beec9b
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-17 19:53:35 -0500

    Job methods tests.

--- spec/features/jobs/user_sees_jobs_at_location_spec.rb
+++ spec/features/jobs/user_sees_jobs_at_location_spec.rb
@@ -7,7 +7,8 @@ describe "User sees all jobs in a city" do
     job = company.jobs.first
     city = job.city
 
-    visit "/jobs?location=#{city}"
+    visit jobs_path
+    click_on city
 
     expect(page).to have_content "All Jobs in #{city}"
     expect(page).to have_content job.title
