c73868e0a021ca4a5d0559c80b6dfb6952212ab7
Ethan Bennett <ebennett19@gmail.com>
2016-12-17 18:39:57 -0700

    Sort of refactor

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,9 @@ class DashboardController < ApplicationController
 
   def index
     format_interest
+    @low_interest = @interest_levels[:low]
+    @medium_interest = @interest_levels[:medium]
+    @high_interest = @interest_levels[:high]
     require 'pry'; binding.pry
   end
 
