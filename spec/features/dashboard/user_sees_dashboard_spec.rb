a3a82157f8c09e1931d4fb1f3ea8042df0ee21ac
Daniel Rodriguez <drod2793@gmail.com>
2016-12-18 16:32:01 -0700

    Adds job count to dashboard page

--- spec/features/dashboard/user_sees_dashboard_spec.rb
+++ spec/features/dashboard/user_sees_dashboard_spec.rb
@@ -10,13 +10,35 @@ RSpec.feature "User sees dashboard" do
 
     visit dashboards_path
 
-    save_and_open_page
-
     expect(page).to have_content(company_1.name)
     expect(page).to have_content(company_2.name)
     expect(page).to have_content(company_4.name)
-    expect(page).to have_content(company_1.level_of_interest)
-    expect(page).to have_content(company_2.level_of_interest)
-    expect(page).to have_content(company_4.level_of_interest)
+    expect(page).to have_content(company_1.average_interest)
+    expect(page).to have_content(company_2.average_interest)
+    expect(page).to have_content(company_4.average_interest)
+  end
+
+  it "user sees count of jobs by interest level" do
+    create_list(:job, 2, level_of_interest: 50)
+    create_list(:job, 4, level_of_interest: 100)
+
+    visit dashboards_path
+
+    expect(page).to have_content(2)
+    expect(page).to have_content(4)
+    expect(page).to have_content(50)
+    expect(page).to have_content(100)
+  end
+
+  it "user sees count of jobs by location" do
+    create_list(:job, 2, city: "Austin")
+    create_list(:job, 4, city: "New York")
+
+    visit dashboards_path
+
+    expect(page).to have_content(2)
+    expect(page).to have_content(4)
+    expect(page).to have_content("Austin")
+    expect(page).to have_content("New York")
   end
 end
