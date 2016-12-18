af66a3fd202a8055648b91c26f28738e7179a2a2
Molly Brown <mollbrown@gmail.com>
2016-12-18 00:15:19 -0700

    adds very rough methods for dashboard

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,15 @@ class Company < ActiveRecord::Base
   validates :name, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :contacts
+
+  def self.company_average_level_of_interest
+    @companies.each do |company|
+      company.jobs.average(:level_of_interest)
+    end
+  end
+  #
+  #   order companes by average level of interest, high to low
+  #   limit to top 3
+  #   show top three company.names and their interest values
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,13 @@ class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
   has_many :comments
+
+  def self.count_by_level_of_interest
+    where(level_of_interest: 75..100).count
+  end
+
+  def self.count_by_location
+    group(:city).count("id").sort_by{ |k,v| v }.reverse
+  end
+
 end
