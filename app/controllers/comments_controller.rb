7b560fe04d8ba0b96d5d3fd25a744c0aa33e9e41
njgheorghita <njgheorghita@email.wm.edu>
2016-12-17 17:22:28 -0700

    Delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,14 @@ class CommentsController < ApplicationController
     redirect_to company_job_path(company.id, job.id)
   end
 
+  def destroy
+    company = Company.find(params[:company_id])
+    job = company.jobs.find(params[:job_id])
+    job.comments.destroy(params[:id])
+
+    redirect_to company_job_path(company.id, job.id)
+  end
+
   private 
 
   def comment_params
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -18,9 +18,23 @@ describe "User sees a specific job" do
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: 3)
 
     visit company_job_path(company, job)
-
     fill_in "comment[content]", with: "this is a comment"
+    click_on "Create Comment"
     expect(page).to have_content("this is a comment")
     expect(Comment.count).to eq(1)
+  end
+
+  scenario "a user deletes a comment for the job" do
+    company = Company.create!(name: "ESPN")
+    Category.create(id:3,title:"HR")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: 3)
+    comment = job.comments.create!(content:"check plz!")
+
+    visit company_job_path(company, job)
+    expect(page).to have_content("check plz!")
+
+    within(".company_#{company.id}") do
+      click_link "Delete"
+    end
   end
 end
