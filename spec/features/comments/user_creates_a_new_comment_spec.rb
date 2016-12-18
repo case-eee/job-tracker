cad3cc772f62845ce313f78f706aceba10f90e85
Robert Smith <robert.smith160@icloud.com>
2016-12-18 12:31:40 -0700

    "Adds test for multiple comments."

--- spec/features/comments/user_creates_a_new_comment_spec.rb
+++ spec/features/comments/user_creates_a_new_comment_spec.rb
@@ -5,14 +5,20 @@ describe "comments#new" do
     @job = create(:job)
     visit(new_company_job_comment_path(@job.company, @job))
     fill_in "comment[content]", with: "Maybe not!"
-
-  end
-  scenario "the user can create a new comment " do
-    expect(@job.comments.count).to eq(0)
-
     click_on("Create Comment")
+  end
 
+  scenario "the user can create a new comment " do
     expect(@job.comments.count).to eq(1)
     expect(@job.comments.first.content).to eq("Maybe not!")
+  end
+
+  scenario "the user can leave multiple comments" do
+    visit(new_company_job_comment_path(@job.company, @job))
+    fill_in "comment[content]", with: "My second comment!"
+    click_on("Create Comment")
+
+    expect(@job.comments.count).to eq(2)
+    expect(@job.comments.last.content).to eq("My second comment!")
   end
 end
