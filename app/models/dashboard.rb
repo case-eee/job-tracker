7603805a136e629e98f7f1b23a0df5eb4ececfca
Daniel Rodriguez <drod2793@gmail.com>
2016-12-18 16:25:32 -0700

    Adds job count by interest level and location to dashboard model

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -10,7 +10,19 @@ class Dashboard
     sorted[0..2]
   end
 
+  def job_locations
+    Job.distinct.pluck(:city).sort
+  end
+
   def job_count_by_location(location)
     Job.where(city: location).count
+  end
+
+  def job_interest_levels
+    Job.distinct.pluck(:level_of_interest).sort
+  end
+
+  def job_count_by_interest_level(interest_level)
+    Job.where(level_of_interest: interest_level).count
   end
 end
--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -21,12 +21,37 @@ describe Dashboard do
     expect(dashboard.top_three_companies_by_interest_level).to eq [company_1, company_4, company_2]
   end
 
-  it "returns count of jobs by location" do
+  it "returns job locations" do
+    create_list(:job, 2, city: "Austin")
+    create_list(:job, 4, city: "New York")
+    dashboard = Dashboard.new
+
+    expect(dashboard.job_locations).to eq ["Austin", "New York"]
+  end
+
+  it "returns count of jobs given location" do
     create_list(:job, 2)
     create_list(:job, 4, city: "New York")
     dashboard = Dashboard.new
 
     expect(dashboard.job_count_by_location("Denver")).to eq 2
     expect(dashboard.job_count_by_location("New York")).to eq 4
+  end
+
+  it "returns interest levels" do
+    create_list(:job, 2, level_of_interest: 50)
+    create_list(:job, 4, level_of_interest: 100)
+    dashboard = Dashboard.new
+
+    expect(dashboard.job_interest_levels).to eq [50, 100]
+  end
+
+  it "returns count of jobs given interest level" do
+    create_list(:job, 2, level_of_interest: 50)
+    create_list(:job, 4, level_of_interest: 100)
+    dashboard = Dashboard.new
+
+    expect(dashboard.job_count_by_interest_level(50)).to eq 2
+    expect(dashboard.job_count_by_interest_level(100)).to eq 4
   end
 end
