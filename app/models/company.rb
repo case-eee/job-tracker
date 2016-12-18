a811ef722a3f355fa9c7d4c0b0f0f1edac0f5f11
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-17 21:57:11 -0500

    Company methods tests.

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,13 @@ class Company < ActiveRecord::Base
   
   has_many :jobs, dependent: :destroy
   has_many :contacts, dependent: :destroy
+
+  def self.average_level_of_interest
+
+  end
+
+  def self.top_companies_by_interest
+    
+  end
+
 end
---
+++ spec/models/company_methods_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "Company" do
+  context "#average_level_of_interest" do
+    scenario "for many jobs" do
+
+      company = create(:company_with_jobs)
+      sum_of_interest = company.jobs.reduce(0) {|sum, job| sum += job.level_of_interest}
+      expected = average_level_of_interest = sum_of_interest.to_f / company.jobs.count
+
+      expect(Company.average_level_of_interest).to eq expected.round(2)
+    end
+
+    scenario "for zero jobs" do
+
+      expect(Company.average_level_of_interest).to eq 0
+    end
+  end
+end
