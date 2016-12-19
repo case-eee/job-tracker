1ac826c5ba50a92f8828a11e46659134b957c4d7
Drew Palazzari <drewpalazzari@hotmail.com>
2016-12-18 18:24:50 -0700

    analytics started

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   end
 
   resources :categories
+
+  get 'jobs' => 'jobs#root'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
