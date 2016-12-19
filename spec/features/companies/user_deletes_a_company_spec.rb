7596863987717903178125e3545dcc96b6a69df1
Daniel Rodriguez <drod2793@gmail.com>
2016-12-18 20:07:27 -0700

    Adds navbar

--- spec/features/companies/user_deletes_a_company_spec.rb
+++ spec/features/companies/user_deletes_a_company_spec.rb
@@ -5,10 +5,7 @@ describe "User deletes existing company" do
     company = Company.create(name: "ESPN")
 
     visit companies_path
-
-    within("ul") do
-      click_link "Delete"
-    end
+    click_link "Delete"
 
     expect(page).to have_content("ESPN was successfully deleted!")
   end
