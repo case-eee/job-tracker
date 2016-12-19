4175ffc9e23859e737ca6c20d9d3977896aabf09
Drew Palazzari <drewpalazzari@hotmail.com>
2016-12-18 21:28:29 -0700

    finishing touches

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,26 +1,31 @@
 # FactoryGirl.define do
-# factory :job do
-#    title              {Faker::Book.title}
-#    description        {Faker::Name.name}
-#    level_of_interest  {Faker::Number.number(2)}
-#    city               {Faker::GameOfThrones.city}
-#    company
-#    category
-#  end
+#   factory :job do
+#     title       {Faker::Book.title}
+#     description {Faker::Name.name}
+#     level_of_interest {Faker::Number.number(2)}
+#     city        {Faker::GameOfThrones.city}
+#     company
+#   end
 #
-#  factory :category do
-#    name
-#  end
+#   factory :category do
+#     name      {Faker::Space.galaxy}
+#   end
 #
-#  factory :company do
-#    sequence :name  do |n|
-#      Faker::Company.name + n.to_s
-#    end
+#   factory :contact do
+#     name      {Faker::Name.name}
+#     position  {Faker::Company.buzzword}
+#     email     {Faker::Internet.email}
+#   end
 #
-#    factory :company_with_jobs do
-#      after(:create) do |company|
-#        create_list(:job, 10, company: company)
-#      end
-#    end
-#  end
+#   factory :company do
+#     sequence :name  do |n|
+#       Faker::Company.buzzword + n.to_s
+#     end
+#
+#     factory :company_with_jobs do
+#       after(:create) do |company|
+#         create_list(:job, 10, company: company)
+#       end
+#     end
+#   end
 # end
