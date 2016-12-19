106fe496a1d636c08424d3080f337a9d73269d4f
Robert Smith <robert.smith160@icloud.com>
2016-12-18 18:27:21 -0700

    "Adds alternate routing"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,13 @@ class HomeController < ApplicationController
   def index
     render :index
   end
+
+  def show
+    @jobs = Job.where(level_of_interest: params[:id])
+    if @jobs.empty?
+      redirect_to companies_path
+    else
+      render :show
+    end
+  end
 end
